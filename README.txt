4ff33ff3f7f1a44cd5cb16618d0b4f48681d48ad
Qijie Zhu <qzhu9@ucsc.edu>
2021-10-09 16:08:38 -0700

    added random flashcard functionality

--- README.txt
+++ README.txt
@@ -2,6 +2,7 @@
 Flashcard App
 -----------------
 Must have Python 3 installed.
+Must have Pandas library for Python 3 installed.
 Do NOT modify any files.
 Double click main.py to run the program.
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter
+import pandas
+import random
 
 CARD_FRONT_IMAGE_PATH = "./images/card_front.png"
 CARD_BACK_IMAGE_PATH = "./images/card_back.png"
@@ -7,6 +9,29 @@ WRONG_BUTTON_IMAGE_PATH = "./images/wrong.png"
 BACKGROUND_COLOR = "#B1DDC6"
 FLASHCARD_CATEGORY_FONT = ("Arial", 40, "italic")
 FLASHCARD_CONTENT_FONT = ("Arial", 60, "bold")
+FLASHCARD_DATA_PATH = "./data/french_words.csv"
+CATEGORY_NAME_FRONT = 'French'
+CATEGORY_NAME_BACK = 'English'
+
+# --------------------- DATA PROCESSING ---------------------- #
+# Create a list of dictionaries called "flashcard_list".
+# Each dictionary looks like: {'French': 'WORD', 'English': 'WORD'}
+flashcard_data = pandas.read_csv(FLASHCARD_DATA_PATH)
+flashcard_list = flashcard_data.to_dict(orient="records")
+
+
+# -------------------- FLASHCARD FUNCTIONALITY --------------- #
+def display_flashcard(new_category_text, new_content_text):
+    flashcard.itemconfig(flashcard_category_text, text=new_category_text)
+    flashcard.itemconfig(flashcard_content_text, text=new_content_text)
+    return
+
+
+def display_random_flashcard():
+    random_flashcard = random.choice(flashcard_list)
+    display_flashcard(CATEGORY_NAME_FRONT, random_flashcard[CATEGORY_NAME_FRONT])
+    return
+
 
 # --------------------- GUI INTERFACE -------------------------#
 # GUI Window
@@ -25,12 +50,14 @@ flashcard.grid(row=1, column=1, columnspan=2, pady=10)
 
 # "Wrong" button
 wrong_button_image = tkinter.PhotoImage(file=WRONG_BUTTON_IMAGE_PATH)
-wrong_button = tkinter.Button(image=wrong_button_image, highlightthickness=0, bd=0, anchor=tkinter.CENTER)
+wrong_button = tkinter.Button(image=wrong_button_image, highlightthickness=0, bd=0, anchor=tkinter.CENTER,
+                              command=display_random_flashcard)
 wrong_button.grid(row=2, column=1, pady=7)
 
 # "Right" button
 right_button_image = tkinter.PhotoImage(file=RIGHT_BUTTON_IMAGE_PATH)
-right_button = tkinter.Button(image=right_button_image, highlightthickness=0, bd=0, anchor=tkinter.CENTER)
+right_button = tkinter.Button(image=right_button_image, highlightthickness=0, bd=0, anchor=tkinter.CENTER,
+                              command=display_random_flashcard)
 right_button.grid(row=2, column=2, pady=7)
 
 # Exit GUI window
