bcdb9a2933064587e29c2a4fb62251f771392180
Qijie Zhu <qzhu9@ucsc.edu>
2021-10-09 15:12:35 -0700

    created flashcard image and buttons

--- main.py
+++ main.py
@@ -11,13 +11,23 @@ BACKGROUND_COLOR = "#B1DDC6"
 window = tkinter.Tk()
 window.title("Flashcards")
 window.minsize(width=800, height=500)
-window.config(padx=20, pady=20, background=BACKGROUND_COLOR)
+window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 
 # Flashcard
-flashcard = tkinter.Canvas(width=700, height=500, highlightthickness=0)
+flashcard = tkinter.Canvas(width=800, height=540, highlightthickness=0, background=BACKGROUND_COLOR)
 flashcard_front_image = tkinter.PhotoImage(file=CARD_FRONT_IMAGE_PATH)
-flashcard.create_image(350, 250, image=flashcard_front_image)
-flashcard.grid(row=1, column=1, columnspan=2)
+flashcard.create_image(400, 270, image=flashcard_front_image, anchor=tkinter.CENTER)
+flashcard.grid(row=1, column=1, columnspan=2, pady=10)
+
+# "Wrong" button
+wrong_button_image = tkinter.PhotoImage(file=WRONG_BUTTON_IMAGE_PATH)
+wrong_button = tkinter.Button(image=wrong_button_image, highlightthickness=0, bd=0, anchor=tkinter.CENTER)
+wrong_button.grid(row=2, column=1, pady=7)
+
+# "Right" button
+right_button_image = tkinter.PhotoImage(file=RIGHT_BUTTON_IMAGE_PATH)
+right_button = tkinter.Button(image=right_button_image, highlightthickness=0, bd=0, anchor=tkinter.CENTER)
+right_button.grid(row=2, column=2, pady=7)
 
 # Exit GUI window
 window.mainloop()
