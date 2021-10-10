630ee669449c51254d5623b3a574919fd65ba335
Qijie Zhu <qzhu9@ucsc.edu>
2021-10-09 17:14:14 -0700

    refactored old code and added flip flashcard functionality

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ BACKGROUND_COLOR = "#B1DDC6"
 FLASHCARD_CATEGORY_FONT = ("Arial", 40, "italic")
 FLASHCARD_CONTENT_FONT = ("Arial", 60, "bold")
 FLASHCARD_DATA_PATH = "./data/french_words.csv"
-CATEGORY_NAME_FRONT = 'French'
-CATEGORY_NAME_BACK = 'English'
+
 
 # --------------------- DATA PROCESSING ---------------------- #
 # Create a list of dictionaries called "flashcard_list".
@@ -19,17 +18,70 @@ CATEGORY_NAME_BACK = 'English'
 flashcard_data = pandas.read_csv(FLASHCARD_DATA_PATH)
 flashcard_list = flashcard_data.to_dict(orient="records")
 
-
 # -------------------- FLASHCARD FUNCTIONALITY --------------- #
-def display_flashcard(new_category_text, new_content_text):
-    flashcard.itemconfig(flashcard_category_text, text=new_category_text)
-    flashcard.itemconfig(flashcard_content_text, text=new_content_text)
+category_name_front = 'French'
+category_name_back = 'English'
+current_flashcard = {}
+current_side = "NONE"
+
+
+def display_current_flashcard():
+    global current_side
+    if current_side == "FRONT":
+        # Change image
+        flashcard.itemconfig(flashcard_image, image=front_image)
+        # Change text
+        flashcard.itemconfig(flashcard_category_text, text=category_name_front)
+        flashcard.itemconfig(flashcard_content_text, text=current_flashcard[category_name_front])
+        # Change text color
+        flashcard.itemconfig(flashcard_category_text, fill='black')
+        flashcard.itemconfig(flashcard_content_text, fill='black')
+    elif current_side == "BACK":
+        # Change image
+        flashcard.itemconfig(flashcard_image, image=back_image)
+        # Change text
+        flashcard.itemconfig(flashcard_category_text, text=category_name_back)
+        flashcard.itemconfig(flashcard_content_text, text=current_flashcard[category_name_back])
+        # Change text color
+        flashcard.itemconfig(flashcard_category_text, fill='white')
+        flashcard.itemconfig(flashcard_content_text, fill='white')
+    else:
+        pass
     return
 
 
-def display_random_flashcard():
+def select_random_flashcard():
+    # Pick random flashcard
     random_flashcard = random.choice(flashcard_list)
-    display_flashcard(CATEGORY_NAME_FRONT, random_flashcard[CATEGORY_NAME_FRONT])
+    # Save the flashcard and set the side to "FRONT"
+    global current_flashcard
+    global current_side
+    current_flashcard = random_flashcard
+    current_side = "FRONT"
+    return
+
+
+def flip_current_card():
+    global current_side
+    if current_side == "FRONT":
+        current_side = "BACK"
+    elif current_side == "BACK":
+        current_side = "FRONT"
+    else:
+        pass
+    return
+
+
+# --------------------- GUI BUTTONS ---------------------------#
+def next_card_button():
+    select_random_flashcard()
+    display_current_flashcard()
+    return
+
+
+def flip_card_button():
+    flip_current_card()
+    display_current_flashcard()
     return
 
 
@@ -41,24 +93,25 @@ window.minsize(width=800, height=500)
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 
 # Flashcard
+front_image = tkinter.PhotoImage(file=CARD_FRONT_IMAGE_PATH)
+back_image = tkinter.PhotoImage(file=CARD_BACK_IMAGE_PATH)
 flashcard = tkinter.Canvas(width=800, height=540, highlightthickness=0, background=BACKGROUND_COLOR)
-flashcard_front_image = tkinter.PhotoImage(file=CARD_FRONT_IMAGE_PATH)
-flashcard.create_image(400, 270, image=flashcard_front_image, anchor=tkinter.CENTER)
-flashcard_category_text = flashcard.create_text(400, 150, text="category", font=FLASHCARD_CATEGORY_FONT)
-flashcard_content_text = flashcard.create_text(400, 270, text="content", font=FLASHCARD_CONTENT_FONT)
+flashcard_image = flashcard.create_image(400, 270, anchor=tkinter.CENTER, image=front_image)
+flashcard_category_text = flashcard.create_text(400, 150, text="Instructions:", font=FLASHCARD_CATEGORY_FONT)
+flashcard_content_text = flashcard.create_text(400, 270, text="Press a button!", font=FLASHCARD_CONTENT_FONT)
 flashcard.grid(row=1, column=1, columnspan=2, pady=10)
 
 # "Wrong" button
 wrong_button_image = tkinter.PhotoImage(file=WRONG_BUTTON_IMAGE_PATH)
 wrong_button = tkinter.Button(image=wrong_button_image, highlightthickness=0, bd=0, anchor=tkinter.CENTER,
-                              command=display_random_flashcard)
+                              command=flip_card_button)
 wrong_button.grid(row=2, column=1, pady=7)
 
 # "Right" button
 right_button_image = tkinter.PhotoImage(file=RIGHT_BUTTON_IMAGE_PATH)
 right_button = tkinter.Button(image=right_button_image, highlightthickness=0, bd=0, anchor=tkinter.CENTER,
-                              command=display_random_flashcard)
+                              command=next_card_button)
 right_button.grid(row=2, column=2, pady=7)
 
-# Exit GUI window
+# GUI Window Mainloop
 window.mainloop()
